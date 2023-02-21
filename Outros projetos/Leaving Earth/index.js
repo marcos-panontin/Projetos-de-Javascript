42ef16c83398d62cdf5cfc5275c3cfb4bb5c454c
Marcos Panontin <amarcospanontin@gmail.com>
2023-02-21 20:13:02 -0300

    Starting Context Menu for Leaving Earth.

--- Outros projetos/Leaving Earth/index.js
+++ Outros projetos/Leaving Earth/index.js
@@ -1,53 +1,86 @@
-// DRAGGIN THE SHIP
+// Calling rightClick function
+document.oncontextmenu = rightClick;
 
-const shipIcon = document.getElementById('shipImg');
-const locations = document.querySelectorAll('.location');
+// // Hiding the menu IF already displayed
+// // document.onclick = hideMenu;
 
-shipIcon.addEventListener('dragstart', dragStart)
+// function hideMenu() {
+//     document.getElementById("contextMenu").style.display = "none";
+// }
 
+function rightClick(e) {
+    // Preventing the default right click menu from popping up
 
-function dragStart(e){
-  console.log('drag starts...');
-  // e.target.classList.add('hide');
+    e.preventDefault();
 
-  e.dataTransfer.setData('text/plain', e.target.id);
+    //MOSTRANDO O MENU
+    const menu = document.getElementById("contextMenu");
+    menu.classList.toggle("displayNone");
+    menu.style.left = e.pageX + "px";
+    menu.style.top = e.pageY + "px";
 
+    // Clicou em nave?
+
+    if (e.target.id === "shipImg") {
+        console.log("Clicou na nave");
+    }
+
+    // Clicou em location?
+    else if (e.target.classList.contains("location")) {
+        console.log("Clicou em location");
+    }
+
+    // }
 }
 
-locations.forEach(location => {
-  location.addEventListener('dragenter', dragEnter);
-  location.addEventListener('dragover', dragOver);
-  location.addEventListener('dragleave', dragLeave);
-  location.addEventListener('drop', drop);
-})
+// CODE FOR DRAGGING THE SHIP (NOT WORKING, DONT KNOW WHY)
 
-function dragEnter(e){
-  e.preventDefault();
-  e.target.classList.add('drag-over');
-}
+// /* draggable element */
+// const shipItem = document.querySelector(".shipImg");
 
-function dragOver(e){
-  e.preventDefault();
-  e.target.classList.add('drag-over');
-}
+// shipItem.addEventListener("dragstart", dragStart);
 
-function dragLeave(e){
-  e.target.classList.remove('drag-over');
-}
+// function dragStart(e) {
+//     e.dataTransfer.setData("text/plain", e.target.id);
+//     setTimeout(() => {
+//         e.target.classList.add("hide");
+//     }, 0);
+// }
 
-function drop(e){
-  e.target.classList.remove('drag-over');
+// /* drop targets */
+// const locations = document.querySelectorAll(".location");
 
-  //get the draggable element
-  const id = e.dataTransfer.getData('text/plain');
-  const draggable = document.getElementById(id);
+// boxes.forEach((box) => {
+//     location.addEventListener("dragenter", dragEnter);
+//     location.addEventListener("dragover", dragOver);
+//     location.addEventListener("dragleave", dragLeave);
+//     location.addEventListener("drop", drop);
+// });
 
-  //add it to the drop target
-  e.target.appendChild(draggable);
-  console.log(e.target)
+// function dragEnter(e) {
+//     e.preventDefault();
+//     e.target.classList.add("drag-over");
+// }
 
-  //unhide :)
-  draggable.classList.remove('hide');
-  draggable.classList.add('red');
+// function dragOver(e) {
+//     e.preventDefault();
+//     e.target.classList.add("drag-over");
+// }
 
-}
+// function dragLeave(e) {
+//     e.target.classList.remove("drag-over");
+// }
+
+// function drop(e) {
+//     e.target.classList.remove("drag-over");
+
+//     // get the draggable element
+//     const id = e.dataTransfer.getData("text/plain");
+//     const draggable = document.getElementById(id);
+
+//     // add it to the drop target
+//     e.target.appendChild(draggable);
+
+//     // display the draggable element
+//     draggable.classList.remove("hide");
+// }
