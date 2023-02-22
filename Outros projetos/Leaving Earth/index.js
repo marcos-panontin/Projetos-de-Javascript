8410ddbaf1c19914210b4c37545fa5a178d33105
Marcos Panontin <amarcospanontin@gmail.com>
2023-02-22 10:36:15 -0300

    Checking overlap between location and ship, in order to be able to display available locations for a ship moving

--- Outros projetos/Leaving Earth/index.js
+++ Outros projetos/Leaving Earth/index.js
@@ -19,18 +19,63 @@ function rightClick(e) {
     menu.style.left = e.pageX + "px";
     menu.style.top = e.pageY + "px";
 
-    // Clicou em nave?
+    // Clicou em nave? - USANDO IF STATEMENT (substituído pelo Switch Statement)
 
-    if (e.target.id === "shipImg") {
-        console.log("Clicou na nave");
-    }
-
-    // Clicou em location?
-    else if (e.target.classList.contains("location")) {
-        console.log("Clicou em location");
-    }
-
+    // if (e.target.id === "shipImg") {
+    //     console.log("Clicou na nave");
     // }
+
+    // // Clicou em location?
+    // else if (e.target.classList.contains("location")) {
+    //     console.log("Clicou em location");
+    // }
+    const targetListaDeClasses = e.target.classList;
+
+    // Checking what class the clicked element contains and triggering the aproppriate response
+
+    switch (true) {
+        case targetListaDeClasses.contains("shipImg"):
+            console.log("Clicou na nave");
+            break;
+
+        case targetListaDeClasses.contains("location"):
+            console.log("Clicou em locação");
+            break;
+    }
+}
+
+//////////////////////////////////////////
+//////// MOVING THE SHIP /////////////////
+//////////////////////////////////////////
+
+// CHECKING IF ELEMENTS OVERLAP - CHECKING WHERE THE SHIP IS
+
+const ship = document.getElementById("shipImg");
+
+//GETTING SHIP LOCATIONS
+
+const shipTop = ship.getBoundingClientRect().top;
+const shipRight = ship.getBoundingClientRect().right;
+const shipBottom = ship.getBoundingClientRect().bottom;
+const shipLeft = ship.getBoundingClientRect().left;
+
+const locationsCollection = Array.from(
+    document.getElementsByClassName("location")
+);
+
+locationsCollection.forEach(checkingOverlap);
+
+// CHECKING WHICH LOCATION MATCHES THE SHIP COORDINATES
+
+function checkingOverlap(location) {
+    if (
+        shipTop > location.getBoundingClientRect().top &&
+        shipBottom < location.getBoundingClientRect().bottom &&
+        shipLeft > location.getBoundingClientRect().left &&
+        shipRight < location.getBoundingClientRect().right
+    ) {
+        console.log(location);
+    }
 }
 
 // CODE FOR DRAGGING THE SHIP (NOT WORKING, DONT KNOW WHY)
