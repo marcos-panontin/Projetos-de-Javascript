1b68a2307c45f579290200705cc4aced1f777fdb
Marcos Panontin <amarcospanontin@gmail.com>
2023-04-05 10:56:26 -0300

    Further work

--- Outros projetos/Genius/script.js
+++ Outros projetos/Genius/script.js
@@ -3,9 +3,9 @@ const startBtn = document.getElementById("start-btn");
 const colorButtons = document.querySelectorAll(".color-button");
 const newGameText = document.getElementById("new-game-text");
 
-let isGameOn = false;
-let colorSequenceLength = 1;
-const randomizedColorsArray = [];
+// DECLARING VARIABLES
+let randomizedColorsArray = [];
+let isGameStarted = false;
 
 const pickRandomColorButton = () =>
   colorButtons[Math.floor(Math.random() * colorButtons.length)];
@@ -13,7 +13,7 @@ const pickRandomColorButton = () =>
 const generateColorSequence = () =>
   randomizedColorsArray.push(pickRandomColorButton());
 
-const illuminateButtons = () => {
+const initialCountdown = () => {
   setTimeout(() => {
     newGameText.textContent = "3";
   }, 200);
@@ -26,37 +26,95 @@ const illuminateButtons = () => {
   setTimeout(() => {
     newGameText.textContent = "";
   }, 800);
-  randomizedColorsArray.forEach((button) => {
+};
+
+const illuminateButtons = () => {
+  console.log(randomizedColorsArray);
+  randomizedColorsArray.forEach((button, index) => {
     setTimeout(() => {
       button.classList.add("change-opacity");
-    }, 1200);
+    }, 1200 * (index + 1));
 
     setTimeout(function () {
       button.classList.remove("change-opacity");
-    }, 1700);
+    }, 1500 + 1200 * (index + 1));
   });
 };
 
-const newGame = () => {
-  isGameOn = true;
-  generateColorSequence();
-  illuminateButtons();
-  let position = 0;
-  let areAnswersCorrect = false;
-  colorButtons.forEach((colorButton) => {
-    colorButton.addEventListener("click", (event) => {
-      if (randomizedColorsArray[position] === event.target) {
-        areAnswersCorrect = true;
-        position += 1;
-      } else if (randomizedColorsArray[position] !== event.target) {
-        console.log('errou!');
-        areAnswersCorrect = false;
-      }
-        if (position === randomizedColorsArray.length && areAnswersCorrect) {
-          console.log("next step");
-        }
+let playerProgress = 0;
+const checkAnswers = (eventTarget) => {
+  console.log("playerProgress - INÍCIO DA FUNÇÃO CHECKANSWERS", playerProgress);
+  if (eventTarget === randomizedColorsArray[playerProgress]) {
+    if (playerProgress === randomizedColorsArray.length - 1) {
+      console.log("novo turno");
+      // SHOWING CHECKMARK IF ALL ANSWERS ARE CORRECT
+      newGameText.textContent = "✓";
+      newGameText.style.fontSize = "2em";
+      // WAITING TO DISAPPEAR THE CHECKMARK AND INITIATING ANOTHER TURN
+      setTimeout(() => {
+        newGameText.textContent = "";
+        newTurn();
+      }, 500);
+    } else {
+      console.log('ACERTOU');
+      playerProgress += 1;
+    }
+
+    //CHECKING IF EVENTTARGET ISNT UNDEFINED AND IS DIFFERENT THAN THE playerProgress IN THE ARRAY
+  } else {
+    console.log(
+      `ERROU! VOCÊ CLICOU EM ${eventTarget.outerHTML} e o esperado era ${randomizedColorsArray[playerProgress].outerHTML}.O playerProgress está em ${playerProgress}.`
+    );
+    // RESETING EVERYTHING
+    randomizedColorsArray = [];
+    playerProgress = 0;
+  }
+
+  console.log("-------------------------------------------");
+};
+
+const listenForClicks = () => {
+  console.log('ENTROU NA LISTENFORCLICKS');
+  colorButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      checkAnswers(event.target);
     });
   });
 };
 
+const newTurn = () => {
+  playerProgress = 0;
+  generateColorSequence();
+  illuminateButtons();
+  listenForClicks();
+};
+
+const newGame = () => {
+  startBtn.disabled = "disabled";
+  isGameStarted = true;
+  initialCountdown();
+  newTurn();
+};
+
 startBtn.addEventListener("click", newGame);
+
+// colorButtons.forEach((colorButton) => {
+//   console.log(randomizedColorsArray, "randomizedCollorsArray");
+
+//   // colorButton.addEventListener("click", (event) => {
+//   //   if (randomizedColorsArray[playerProgress] === event.target) {
+//   //     console.log(randomizedColorsArray, "randomizedCollorsArray");
+//   //     console.log("resposta correta");
+//   //     areAnswersCorrect = true;
+//   //     playerProgress += 1;
+//   //   } else if (randomizedColorsArray[playerProgress] !== event.target) {
+//   //     console.log("resposta errada");
+//   //     areAnswersCorrect = false;
+//   //   }
+//   //   if (playerProgress === randomizedColorsArray.length && areAnswersCorrect) {
+//   //     console.log("playerProgress = array.length; all answers are correct");
+//   //     return true;
+//   //   }
+// });
+// });
+// };
